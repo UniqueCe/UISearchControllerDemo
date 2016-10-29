f0e17ae79aac2a15a579cd517cb7e39214521698
UniqueCe <15011529145@163.com>
2016-10-29 15:37:12 +0800

    创建SearchDataController

---
+++ UISearchControllerDemo/UISearchControllerDemo/LPCSearchDataTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LPCSearchDataTableViewController.h
+//  UISearchControllerDemo
+//
+//  Created by 刘培策 on 16/10/29.
+//  Copyright © 2016年 UniqueCe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPCSearchDataTableViewController : UITableViewController
+
+@end
---
+++ UISearchControllerDemo/UISearchControllerDemo/LPCSearchDataTableViewController.m
@@ -0,0 +1,46 @@
+//
+//  LPCSearchDataTableViewController.m
+//  UISearchControllerDemo
+//
+//  Created by 刘培策 on 16/10/29.
+//  Copyright © 2016年 UniqueCe. All rights reserved.
+//
+
+#import "LPCSearchDataTableViewController.h"
+
+@interface LPCSearchDataTableViewController ()
+
+@end
+
+@implementation LPCSearchDataTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+#pragma mark - Table view data source
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+
+
+@end
